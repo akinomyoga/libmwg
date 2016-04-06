c2a33ec41caa5f095a77f533852826797ff494cb
Koichi Murase <myoga.murase@gmail.com>
2016-04-06 13:04:16 +0900

    mwg/std/chrono: \new mwg::stdm::chrono::{system,steady,high_resolution}_clock; mwg/string3/str.bench1.cpp
    * string3/i001-bench1.cpp: i001-bench2.cpp を統合、実際に計測を行う様に書き換え。\rename -> string3/str.bench1.cpp.
    * mwg/std/chrono: \new system_clock, steady_clock, high_resolution_clock (simple implementation with <ctime>).
    * mwg/std/chrono: \new time_point::min, time_point::max
    * mwg/std/chrono: \new operator-(time_point,time_point)
    * mwg/std/chrono: \new common_type<time_point,time_point>
    * mwg/std/chrono: C++14 constexpr functions.
    * mwg/std/chrono: \new C++17 floor(duration), ceil(duration), round(duration), floor(time_point), ceil(time_point), round(time_point).
    * mwg/std/chrono (mwg::stdm::duration): \remove non standard constructors.
    * mwg/std/type_traits (mwg::stdm::common_type): support common types of arithmetic types.

---
+++ src/mwg/string3/str.bench1.cpp
@@ -0,0 +1,59 @@
+#include <mwg/std/chrono>
+#include <mwg/str.h>
+#include <string>
+#include <iostream>
+
+namespace chrono=mwg::stdm::chrono;
+
+struct scope_stopwatch{
+  const char* message;
+  chrono::high_resolution_clock::time_point start;
+  scope_stopwatch(const char* message)
+    :message(message),
+     start(chrono::high_resolution_clock::now())
+  {}
+  ~scope_stopwatch(){
+    chrono::high_resolution_clock::time_point const finish = chrono::high_resolution_clock::now();
+    chrono::microseconds const us = chrono::duration_cast<chrono::microseconds>(finish-start);
+    std::cout << message << ": " << us.count() << "us\n";
+  }
+};
+
+namespace string_bench{
+  int test_compare1a(){
+    int r=0;
+    if(mwg::str("hello")=="hello")r++;
+    if(mwg::str("hello")=="world")r++;
+    if(mwg::str("world")=="hello")r++;
+    return r;
+  }
+
+  int test_compare1b(){
+    int r=0;
+    if(std::string("hello")=="hello")r++;
+    if(std::string("hello")=="world")r++;
+    if(std::string("world")=="hello")r++;
+    // if(std::string("hello")=="hello")r++;
+    // if(std::string("hello")=="world")r++;
+    // if(std::string("world")=="hello")r++;
+    return r;
+  }
+}
+
+int main(){
+  int a=0;
+
+  {
+    scope_stopwatch sw("test_compare1a");
+    for(int i=0;i<10000;i++)
+      a+=string_bench::test_compare1a();
+  }
+
+  {
+    scope_stopwatch sw("test_compare1b");
+    for(int i=0;i<10000;i++)
+      a-=string_bench::test_compare1b();
+  }
+
+  return a==0?0:1;
+}
