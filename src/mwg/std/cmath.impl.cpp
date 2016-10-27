b440f2433fe67a612a594c06a5488f84e0c91ef5
Koichi Murase <myoga.murase@gmail.com>
2016-10-27 10:01:16 +0900

    mwg/std/cmath: add mwg::stdm::{,l,ll}{rint,round}

--- src/mwg/std/cmath.impl.cpp
+++ src/mwg/std/cmath.impl.cpp
@@ -72,9 +72,9 @@ namespace cmath_detail {
     }
   }
 
-  int logb(float value) {return logb_impl(value);}
-  int logb(double value) {return logb_impl(value);}
-  int logb(long double value) {return logb_impl(value);}
+  float logb(float value) {return logb_impl(value);}
+  double logb(double value) {return logb_impl(value);}
+  long double logb(long double value) {return logb_impl(value);}
 
 }
 }
@@ -102,9 +102,9 @@ namespace cmath_detail {
     int const exp = mwg::stdm::logb(from);
     T man = mwg::stdm::scalbn(from, -exp);
     if (from < to)
-      man += mwg::stdm::numeric_limits<T>::epsilon();
+      man += std::numeric_limits<T>::epsilon();
     else
-      man -= mwg::stdm::numeric_limits<T>::epsilon();
+      man -= std::numeric_limits<T>::epsilon();
     T const next = mwg::stdm::scalbn(man, exp);
 
     if (mwg::stdm::isinf(next))
@@ -118,6 +118,79 @@ namespace cmath_detail {
   float nextafter(float lhs, float rhs) {return nextafter_impl(lhs, rhs);}
   double nextafter(double lhs, double rhs) {return nextafter_impl(lhs, rhs);}
   long double nextafter(long double lhs, long double rhs) {return nextafter_impl(lhs, rhs);}
+
+}
+}
+}
+#endif
+
+#if !defined(MWGCONF_HAS_STD_RINT) && !defined(MWGCONF_HAS_RINT)
+# include <mwg/std/cfenv>
+namespace mwg {
+namespace stdm {
+namespace cmath_detail {
+
+  template<typename T>
+  static T rint_impl(T value) {
+    switch (fegetround()) {
+    case FE_DOWNWARD:
+      return std::floor(value);
+    case FE_UPWARD:
+      return std::ceil(value);
+    case FE_TOWARDZERO:
+      return mwg::stdm::trunc(value);
+    case FE_TONEAREST:
+    default:
+      return mwg::stdm::round(value);
+    }
+  }
+
+  float rint(float value) {return rint_impl(value);}
+  doble rint(double value) {return rint_impl(value);}
+  long double rint(long double value) {return rint_impl(value);}
+
+}
+}
+}
+#endif
+
+#if defined(MWG_STDM_CMATH_Defines_lint) || defined(MWG_STDM_CMATH_Defines_llint)
+# include <mwg/std/cfenv>
+# include <mwg/std/limits>
+namespace mwg {
+namespace stdm {
+namespace cmath_detail {
+
+  template<typename R, typename T>
+  static R lint_impl(T value) {
+    if (mwg::stdm::isnan(value)) {
+      feraiseexcept(FE_INVALID);
+      return (R) 0;
+    }
+
+    if (value < mwg::stdm::numeric_limits<R>::lowest()) {
+      feraiseexcept(FE_INVALID);
+      return mwg::stdm::numeric_limits<R>::lowest();
+    }
+
+    if (mwg::stdm::numeric_limits<R>::max() < value) {
+      feraiseexcept(FE_INVALID);
+      return mwg::stdm::numeric_limits<R>::max();
+    }
+
+    return (R) value;
+  }
+
+# ifdef MWG_STDM_CMATH_Defines_lint
+  long lint(float value) {return lint_impl<long>(value);}
+  long lint(double value) {return lint_impl<long>(value);}
+  long lint(long double value) {return lint_impl<long>(value);}
+# endif
+# ifdef MWG_STDM_CMATH_Defines_llint
+  long long llint(float value) {return lint_impl<long long>(value);}
+  long long llint(double value) {return lint_impl<long long>(value);}
+  long long llint(long double value) {return lint_impl<long long>(value);}
+# endif
 
 }
 }
