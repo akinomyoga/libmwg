4cb3d8fe459b6a9c62d02949772412ed9cbf5492
Koichi Murase <myoga.murase@gmail.com>
2017-02-06 00:12:45 +0900

    standard spaces: mwg/range.h

--- src/mwg/range.h
+++ src/mwg/range.h
@@ -7,38 +7,38 @@
 #include <mwg/concept.h>
 #include <mwg/functor.h>
 
-namespace mwg{
+namespace mwg {
 //==============================================================================
 
-namespace range_detail{
-  template<typename F,typename T>
-  struct foreach_switch{
+namespace range_detail {
+  template<typename F, typename T>
+  struct foreach_switch {
     static const int value=
-      mwg::be_functor<F,bool(const T&)>::value?1:
+      mwg::be_functor<F, bool(const T&)>::value? 1:
       0;
   };
 
-  template<typename T,int S>
-  struct range_traits_impl{
+  template<typename T, int S>
+  struct range_traits_impl {
     typedef T value_type;
     typedef int difference_type;
-    static void increment_inclusive(T& end){end++;}
+    static void increment_inclusive(T& end) {end++;}
   };
   template<typename T>
-  struct range_traits_impl<T,1>{
+  struct range_traits_impl<T, 1> {
     // for floating-point numbers
     typedef T value_type;
     typedef T difference_type;
-    static void increment_inclusive(T& /* end */){}
+    static void increment_inclusive(T& /* end */) {}
   };
 }
 
 template<typename T>
 struct range_traits
-  :range_detail::range_traits_impl<T,mwg::stdm::is_floating_point<T>::value?1:0>{};
+  :range_detail::range_traits_impl<T, mwg::stdm::is_floating_point<T>::value? 1: 0> {};
 
 template<typename T>
-class range_base{
+class range_base {
   typedef range_traits<T> traits_type;
   typedef T value_type;
   typedef typename traits_type::difference_type difference_type;
@@ -46,59 +46,59 @@ private:
   value_type m_begin;
   value_type m_end;
 public:
-  value_type begin() const{return this->m_begin;}
-  value_type end() const{return this->m_end;}
+  value_type begin() const {return this->m_begin;}
+  value_type end() const {return this->m_end;}
 public:
-  range_base(const value_type& begin,const value_type& end,bool inclusive=false)
-    :m_begin(begin),m_end(end)
+  range_base(const value_type& begin, const value_type& end, bool inclusive = false)
+    :m_begin(begin), m_end(end)
   {
-    if(inclusive)
+    if (inclusive)
       traits_type::increment_inclusive(m_end);
   }
-  difference_type length() const{
-    return this->m_end-this->m_begin;
+  difference_type length() const {
+    return this->m_end - this->m_begin;
   }
-  bool is_empty() const{
-    return this->length()<=0;
+  bool is_empty() const {
+    return this->length() <= 0;
   }
-  bool contains(const value_type& value) const{
-    return this->m_begin<=value&&value<this->m_end;
+  bool contains(const value_type& value) const {
+    return this->m_begin <= value && value < this->m_end;
   }
-  bool contains(const range_base& r) const{
-    return this->m_begin<=r.m_begin&&r.m_end<=this->m_end;
+  bool contains(const range_base& r) const {
+    return this->m_begin <= r.m_begin && r.m_end <= this->m_end;
   }
   //--------------------------------------------------------------------------
-  bool operator==(const range_base& r) const{
-    return m_begin==r.m_begin&&m_end==r.m_end;
+  bool operator==(const range_base& r) const {
+    return m_begin == r.m_begin && m_end == r.m_end;
   }
   // //--------------------------------------------------------------------------
   // template<typename F>
-  // typename stdm::enable_if<range_detail::foreach_switch<F,T>::value==0,void>::type
-  // foreach(const F& _f) const{
-  //   F& f=const_cast<F&>(_f);
-  //   for(value_type i=m_begin;i<m_end;i++)
-  //     mwg::functor_invoke<void(const value_type&)>(f,i);
+  // typename stdm::enable_if<range_detail::foreach_switch<F, T>::value == 0, void>::type
+  // foreach(const F& _f) const {
+  //   F& f = const_cast<F&>(_f);
+  //   for (value_type i = m_begin; i < m_end; i++)
+  //     mwg::functor_invoke<void(const value_type&)>(f, i);
   // }
   // template<typename F>
-  // typename stdm::enable_if<range_detail::foreach_switch<F,T>::value==1,void>::type
-  // foreach(const F& _f mwg_gcc3_concept_overload(1)) const{
-  //   F& f=const_cast<F&>(_f);
-  //   for(value_type i=m_begin;i<m_end;i++)
-  //     if(!mwg::functor_invoke<bool(const value_type&)>(f,i))break;
+  // typename stdm::enable_if<range_detail::foreach_switch<F, T>::value == 1, void>::type
+  // foreach(const F& _f mwg_gcc3_concept_overload(1)) const {
+  //   F& f = const_cast<F&>(_f);
+  //   for (value_type i = m_begin; i < m_end; i++)
+  //     if (!mwg::functor_invoke<bool(const value_type&)>(f, i)) break;
   // }
 };
 
 template<typename T>
-class range:public range_base<T>{
+class range: public range_base<T> {
 public:
-  range():range_base<T>(0,0){}
-  range(const T& begin,const T& end,bool inclusive=false)
-    :range_base<T>(begin,end,inclusive){}
+  range(): range_base<T>(0, 0) {}
+  range(const T& begin, const T& end, bool inclusive = false)
+    :range_base<T>(begin, end, inclusive) {}
 public:
-  range operator&(const range_base<T>& r) const{
+  range operator&(const range_base<T>& r) const {
     return new range(
-      std::max(this->m_begin,r.m_begin),
-      std::min(this->m_end,r.m_end)
+      std::max(this->m_begin, r.m_begin),
+      std::min(this->m_end, r.m_end)
     );
   }
 };
@@ -107,18 +107,18 @@ public:
 //  mwg::make_range
 //    指定した引数から range を生成します。
 //------------------------------------------------------------------------------
-template<typename T,int N>
-range<T*> make_range(T (&arr)[N]){
-  return range<T>(arr,arr+N);
+template<typename T, int N>
+range<T*> make_range(T (&arr)[N]) {
+  return range<T>(arr, arr + N);
 }
 template<typename T>
-range<T> make_range(const T& begin,const T& end,bool inclusive=false){
-  return range<T>(begin,end,inclusive);
+range<T> make_range(const T& begin, const T& end, bool inclusive = false) {
+  return range<T>(begin, end, inclusive);
 }
 template<typename T>
-typename stdm::enable_if<!mwg::stdm::is_same<T,int>::value,range<T> >::type
-make_range(const T& begin,int len){
-  return range<T>(begin,begin+len);
+typename stdm::enable_if<!mwg::stdm::is_same<T, int>::value, range<T> >::type
+make_range(const T& begin, int len) {
+  return range<T>(begin, begin + len);
 }
 
 typedef range<int> irange;
@@ -126,8 +126,8 @@ typedef range<int> irange;
 //------------------------------------------------------------------------------
 
 template<typename T>
-T clamp(T const& value,mwg::range<T> r){
-  return (value<r.begin())?r.begin(): (value>r.end())?r.end(): value;
+T clamp(T const& value, mwg::range<T> r) {
+  return (value < r.begin())? r.begin(): (value > r.end())? r.end(): value;
 }
 
 //==============================================================================
