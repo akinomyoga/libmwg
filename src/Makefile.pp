akinomyoga/libmwg

commit 11be5edd71f5b31f2a1a4fd449bb337e5b57171d

Makefile.pp: fix install directory of mwg/config.h

src/Makefile.pp
@@ -113,7 +113,7 @@ source_files+=$(CFGDIR)/include/mwg_config.stamp $(CPPDIR)/mwg/config.h
 install_files+=$(INS_INCCFG)/mwg_config.h $(INS_INCCFG)/mwg/config.h
 $(INS_INCCFG)/mwg_config.h: $(CFGDIR)/include/mwg_config.stamp
 	@echo 'INS header $(CFGDIR)/include/mwg_config.h'; $(MMAKECMD) install-header $(CFGDIR)/include/mwg_config.h $@
-$(INS_INCCFG)/mwg/config.h: $(CPPDIR)/mwg/config.h
+$(INS_INCDIR)/mwg/config.h: $(CPPDIR)/mwg/config.h
 	@echo 'INS header $<'; $(MMAKECMD) install-header $< $@
 
 $(CFGDIR)/libmwg.a: $(object_files)

commit 404e5264cdccd8bd6da0640fc18e7f9870a121ed

src/Makefile.pp: fix install directory of mwg/config.h (inconsistensy introduced by 11be5ed)

src/Makefile.pp
@@ -110,7 +110,7 @@ $(CFGDIR)/include/mwg_config_common.h: $(CFGDIR)/include/mwg_config.1.h
 $(CPPDIR)/mwg/config.h: mwg_config.mconf | $(CFGDIR)/include/mwg_config_common.h $(CPPDIR)/mwg
 	cp $(CFGDIR)/include/mwg_config_common.h $@
 source_files+=$(CFGDIR)/include/mwg_config.stamp $(CPPDIR)/mwg/config.h
-install_files+=$(INS_INCCFG)/mwg_config.h $(INS_INCCFG)/mwg/config.h
+install_files+=$(INS_INCCFG)/mwg_config.h $(INS_INCDIR)/mwg/config.h
 $(INS_INCCFG)/mwg_config.h: $(CFGDIR)/include/mwg_config.stamp
 	@echo 'INS header $(CFGDIR)/include/mwg_config.h'; $(MMAKECMD) install-header $(CFGDIR)/include/mwg_config.h $@
 $(INS_INCDIR)/mwg/config.h: $(CPPDIR)/mwg/config.h