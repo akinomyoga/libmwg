e8426bcbf102b31ec07055b948e18fabffef5ba8
Koichi Murase <myoga.murase@gmail.com>
2017-07-24 09:56:00 +0900

    mwg/char.h: add constexpr

--- src/mwg/char.h
+++ src/mwg/char.h
@@ -13,33 +13,38 @@ namespace mwg{
   template<typename T, int CP>
   class char_t {
     T value;
+
   public:
-    char_t(): value(0){}
-    char_t(const char_t& ch): value(ch.value) {}
-    char_t(const char_data<T, CP>& ch): value(ch.value) {}
-    char_t(const T& ch): value(ch) {}
-    explicit char_t(int ch): value(ch) {}
-    operator T() const {return this->value;}
-    operator bool() const {return this->value != 0;}
-    operator char_data<T, CP>() const {char_data<T, CP> ret = {this->value}; return ret;}
+    mwg_constexpr char_t(): value(0) {}
+    mwg_constexpr char_t(const char_t& ch): value(ch.value) {}
+    mwg_constexpr char_t(const char_data<T, CP>& ch): value(ch.value) {}
+    mwg_constexpr char_t(const T& ch): value(ch) {}
+    mwg_constexpr explicit char_t(int ch): value(ch) {}
+    mwg_constexpr operator T() const {return this->value;}
+    mwg_constexpr operator char_data<T, CP>() const {char_data<T, CP> ret = {this->value}; return ret;}
+
+    mwg_constexpr operator bool() const {return this->value != 0;}
+    mwg_constexpr operator!() const {return this->value == 0;}
+
   public:
-    char_t& operator=(const char_t& ch) {this->value = ch.value; return *this;}
-    char_t& operator=(const T& ch) {this->value = ch; return *this;}
-    char_t& operator+=(const T& d) {this->value += d; return *this;}
-    char_t& operator-=(const T& d) {this->value -= d; return *this;}
+    mwg_constexpr char_t& operator=(const char_t& ch) {this->value = ch.value; return *this;}
+    mwg_constexpr char_t& operator=(const T& ch) {this->value = ch; return *this;}
+    mwg_constexpr char_t& operator+=(const T& d) {this->value += d; return *this;}
+    mwg_constexpr char_t& operator-=(const T& d) {this->value -= d; return *this;}
+
   public:
-    char_t operator+(const T& d) const {return this->value + d;}
-    char_t operator-(const T& d) const {return this->value - d;}
-    T operator-(const char_t& ch) const {return this->value - ch.value;}
-    bool operator==(const char_t& ch) const {return this->value == ch.value;}
-    bool operator!=(const char_t& ch) const {return this->value != ch.value;}
-    bool operator<=(const char_t& ch) const {return this->value <= ch.value;}
-    bool operator>=(const char_t& ch) const {return this->value >= ch.value;}
-    bool operator< (const char_t& ch) const {return this->value <  ch.value;}
-    bool operator> (const char_t& ch) const {return this->value >  ch.value;}
+    mwg_constexpr char_t operator+(const T& d) const {return this->value + d;}
+    mwg_constexpr char_t operator-(const T& d) const {return this->value - d;}
+    mwg_constexpr T operator-(const char_t& ch) const {return this->value - ch.value;}
+    mwg_constexpr bool operator==(const char_t& ch) const {return this->value == ch.value;}
+    mwg_constexpr bool operator!=(const char_t& ch) const {return this->value != ch.value;}
+    mwg_constexpr bool operator<=(const char_t& ch) const {return this->value <= ch.value;}
+    mwg_constexpr bool operator>=(const char_t& ch) const {return this->value >= ch.value;}
+    mwg_constexpr bool operator< (const char_t& ch) const {return this->value <  ch.value;}
+    mwg_constexpr bool operator> (const char_t& ch) const {return this->value >  ch.value;}
   };
   template<typename T, int CP>
-  char_t<T, CP> operator+(const T& d, const char_t<T,CP>& c) {return c.value + d;}
+  mwg_constexpr char_t<T, CP> operator+(const T& d, const char_t<T,CP>& c) {return c.value + d;}
 
   static const i4t MS_CP  = 1 << 16;
   static const i4t IBM_CP = 2 << 16;
